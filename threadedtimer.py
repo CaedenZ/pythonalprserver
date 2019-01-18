a9fbcfcd083eb13142283cd0086b057cdcabf46d
ubuntu <fengye.16@ichat.sp.edu.sg>
2019-01-18 15:30:22 +0800

    completed threadedtimer.py

--- threadedtimer.py
+++ threadedtimer.py
@@ -1,22 +1,29 @@
+import threading
+import time
+
+
 class threadedtimer:
     def __init__(self, duration):
-        self.start = 0
+        self.timeRan = 0
         self.duration = duration
         self.exceeded = False
+        self.stopped = False
 
     def start(self):
         threading.Thread(target=self.startTimer, args=()).start()
         return self
 
-    def begin(self):
-        if(start >= duration) {
-            self.exceeded = True
-        }
-        start += 1
+    def startTimer(self):
+        while not self.stopped:
+            if(self.timeRan <= self.duration):
+                start += 1
+            else if not self.exceeded:  # If we have not set self.exceeded to True yet
+                self.exceeded = True
 
     def reset(self):
-        self.start = 0
+        self.timeRan = 0
         self.exceeded = False
-
+        self.stopped = False
 
     def stop(self):
+        self.stopped = True
